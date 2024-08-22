5aebb0a21cb2d47b84aa9f5ccdea06383a32deea
Antoine27992 <65226717+Antoine27992@users.noreply.github.com>
2024-08-22 21:36:25 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,29 +1,31 @@
-function showPaintings(){
+function showPaintings() {
     t = document.getElementById("table");
     var table = document.createElement('table');
     var index = 1;
 
-    for(var r=0; r < 5;r++){
-        var tr = document.createElement('tr');        
-        for(var c=0; c < 5;c++){
+    for (var r = 0; r < 6; r++) {
+        var tr = document.createElement('tr');
+        for (var c = 0; c < 5; c++) {
             var img = document.createElement('img');
             var td = document.createElement('td');
             var image;
-            img.setAttribute("class","landscape");
-            if (index < 10){
+            if (index >=1 && index <=20){
+                img.setAttribute("class", "landscape");
+            }else{
+                img.setAttribute("class", "portrait");
+            }
+            if (index < 10) {
                 image = "img/bild00" + (index) + ".jpg";
-            } else if (index >= 10 && index < 30) {
+            } else if (index >= 10 && index <= 30) {
                 image = "img/bild0" + (index) + ".jpg";
-            }            
-            img.setAttribute("src",image);
-            td.style.width ="25px";
-            td.style.height = "25px";
+            }
+            img.setAttribute("src", image);
             td.appendChild(img);
             tr.appendChild(td);
             index++;
         }
         table.appendChild(tr);
-    }    
+    }
     t.appendChild(table);
 }
 
